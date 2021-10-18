1ea8a757c6f696e8ecb134080722d2dbed84c662
Colin Bethea <colinjbethea@gmail.com>
2021-10-18 13:27:46 -0400

    Initial commit; with Java

---
+++ java/Demo.java
@@ -0,0 +1,14 @@
+/* Imports */
+import java.util.ArrayList; // Imports (ArrayList) package within java.util
+import java.security.*; // Imports (*) packages within java.security
+import java.util.Scanner; // Imports (Scanner) package within java.util
+
+/* Class */
+public class Demo {
+  /* Entry Point (main) */
+  public static void main(String[] args) {
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i]);
+    }
+  }
+}
